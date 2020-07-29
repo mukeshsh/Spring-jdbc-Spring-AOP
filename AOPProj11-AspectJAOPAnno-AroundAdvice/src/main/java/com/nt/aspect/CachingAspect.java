ec80c0316f766ef54a4b1e38ea7f64b5b72df7dc
Mukesh Patel <patelmukesh325@gmail.com>
2020-07-29 16:27:26 +0530

    Annotation based aspectjAOP

---
+++ AOPProj11-AspectJAOPAnno-AroundAdvice/src/main/java/com/nt/aspect/CachingAspect.java
@@ -0,0 +1,34 @@
+package com.nt.aspect;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+@Component("cachingAspect")
+@Aspect
+public class CachingAspect {
+	private Map<String, Object> cacheMap=new HashMap();
+	@Around("execution(float com.nt.service.ECommerceStore.calcBillAmount(..))")
+	@Order(2)
+	public Object caching(ProceedingJoinPoint pjp)throws Throwable{
+		String key=null;
+		Object retVal=null;
+
+		key=pjp.getSignature()+Arrays.deepToString(pjp.getArgs());
+		if(!cacheMap.containsKey(key)) {
+			retVal=pjp.proceed();
+			cacheMap.put(key, retVal);
+			System.out.println("From target");
+		}
+		else {
+			retVal=cacheMap.get(key);
+			System.out.println("From Cache");
+		}
+		return retVal;
+	}
+}
