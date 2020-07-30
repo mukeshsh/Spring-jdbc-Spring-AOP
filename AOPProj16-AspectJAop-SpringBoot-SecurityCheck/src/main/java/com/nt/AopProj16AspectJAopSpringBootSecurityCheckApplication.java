d70407559e889e5e8408b99d087084fc6ba756b1
Mukesh Patel <patelmukesh325@gmail.com>
2020-07-30 10:13:55 +0530

    Spring Boot

---
+++ AOPProj16-AspectJAop-SpringBoot-SecurityCheck/src/main/java/com/nt/AopProj16AspectJAopSpringBootSecurityCheckApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopProj16AspectJAopSpringBootSecurityCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopProj16AspectJAopSpringBootSecurityCheckApplication.class, args);
+	}
+
+}
